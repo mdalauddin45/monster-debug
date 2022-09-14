b83741f6f56fe9dc83d5b2194dcde11fe47bbaf2
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-14 21:00:22 +0600

    Time out display clr

--- script.js
+++ script.js
@@ -110,17 +110,19 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1> ${count} </h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
-    count--;
+    --count;
+
   }, 1000);
+
 };
 
 // START Countdown
