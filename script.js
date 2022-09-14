6a6cf405a0bff09a502f8a5233c1e9e49ba0fffe
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-14 21:37:57 +0600

    show time seconds clr

--- script.js
+++ script.js
@@ -137,5 +137,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime.toFixed(0) ? timeSpent.toFixed(0) : 0} seconds`;
 }, 1000);
