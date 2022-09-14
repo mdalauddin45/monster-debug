e763d4b18037d7785c34f11d57fb35ba7fa7f0c9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-15 00:04:08 +0600

    second set

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = ((finishTime - startTime) / 1000).toFixed(0);
 
   // show result modal
   resultModal.innerHTML = "";
